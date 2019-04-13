fe5474d98822d99a6da00ef63c93ec4964ec5741
jmanc3 <jman12316@live.com>
2019-04-13 16:59:51 -0500

    Got rid of the splash screen on launch

--- src/scv3/Main.java
+++ src/scv3/Main.java
@@ -13,21 +13,18 @@ import org.controlsfx.control.NotificationPane;
 
 public class Main extends Application {
     public static NotificationPane notificationPane;
-    public static Parent splashRoot;
     public static Parent mainRoot;
 
     @Override
     public void start(Stage stage) throws Exception {
-        splashRoot = FXMLLoader.load(getClass().getResource("fxml/splash.fxml"));
         mainRoot = FXMLLoader.load(getClass().getResource("fxml/SCV3.fxml"));
 
         notificationPane = new NotificationPane(mainRoot);
         notificationPane.getStylesheets().add(getClass().getResource("assets/materialfx.css").toExternalForm());
         notificationPane.setShowFromTop(false);
 
-        Scene scene = new Scene(splashRoot);
+        Scene scene = new Scene(mainRoot);
 
-        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
@@ -35,7 +32,7 @@ public class Main extends Application {
     public static void message(String text, double time) {
         notificationPane.show(text);
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(time * 1000),
-                                                      ae -> notificationPane.hide()));
+                ae -> notificationPane.hide()));
         timeline.play();
     }
 
